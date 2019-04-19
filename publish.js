d117e6bd52a5cb474c38caae8dff4732e0925074
xx <xinxiong@enoch-car.com>
2019-04-19 13:55:40 +0800

    ~

--- publish.js
+++ publish.js
@@ -1,3 +1,10 @@
 const fs = require('fs-extra')
 
-fs.copySync('dist', 'wwwroot')
+fs.copySync(
+  'dist',
+  'wwwroot', {
+    filter(src) {
+      return src.endsWith('exe') || src.endsWith('yml')
+    }
+  }
+)
